101eebb388816b931fd1725d1c85a060e3b06aad
CAMELNINGA <74087876+CAMELNINGA@users.noreply.github.com>
2021-11-20 22:21:18 +0300

    load and save page

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+//save_page
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{
+		Title: title,
+		Body:  body}, nil
+}
 
 func main() {
-	fmt.Println("Hi")
+	p1 := &Page{Title: "TestPage", Body: []byte("This is shit work")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
 }
